8615f9270d3f3134a3db8ef6b1e1a0c0d5cae7cf
nebojsadj <djordjevicn87@gmail.com>
2020-10-06 11:05:35 +0200

    correction css

--- src/components/City.js
+++ src/components/City.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function City({ state, error }) {
   return (
-    <div className="conteiner">
+    <div className="container">
       <div className="row mt-5">
         <div className="col-8 offset-2">
           <h1 className="text-center display-4 text-light">
@@ -11,9 +11,7 @@ function City({ state, error }) {
           <div className="row">
             <div className="col-10 offset-1 ">
               <img
-                src={`http://openweathermap.org/img/wn/${
-                  state.icon || "01n"
-                }@2x.png`}
+                src={`http://openweathermap.org/img/wn/${state.icon}@2x.png`}
                 alt="wthr img"
                 className="mx-auto d-block"
               />
@@ -21,13 +19,13 @@ function City({ state, error }) {
                 {`${state.temp} ° C`}
               </h3>
               <div className="row">
-                <div className="col-6 offset-3 mt-3">
+                <div className="col-8 offset-2 mt-3">
                   <h3 className="mt-4 text-light float-left">{`min ${state.min} ° C`}</h3>
                   <h3 className="mt-4 text-light float-right">{`min ${state.max} ° C`}</h3>
                 </div>
               </div>
 
-              <h3 className="text-center text-light mt-4">
+              <h3 className="text-center text-light mt-5">
                 {state.description}
               </h3>
             </div>
